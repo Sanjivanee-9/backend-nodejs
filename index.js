f9b2ef346fac1514f04ccee6b2cb50e7c8dd62b0
Sanjivanee-9 <sanjivaneesj18@gmail.com>
2023-12-26 11:25:55 +0530

    modified:   index.js 	modified:   test.js

--- index.js
+++ index.js
@@ -1,2 +1,15 @@
-console.log("Sanjivanee RAIT Internship")
-console.log("2023")
+var http = require('http');
+var data = require('./test')
+
+http.createServer(function(req,res){
+    // res.writeHead(200,{'Content-Type': 'application/json'})
+    res.writeHead(200,{'Content-Type': 'text/html'})
+    res.write("<h1>RAIT</h1>" + data.testModule())
+    res.end('<h3>welcome to rait..</h3>')
+}).listen(8787);
+
+
+
+
+// http://192.168.1.1/
+
---
+++ test.js
@@ -0,0 +1,3 @@
+exports.testModule = function () {
+    return "this is test module";
+}
